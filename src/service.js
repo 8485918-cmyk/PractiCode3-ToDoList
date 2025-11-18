bd4d4cca734ee013677b4754e885caa8af832220
Malka Noyman <8485918@gmail.com>
2025-11-18 22:12:54 +0200

    שינויים לפני rebase

---
+++ src/service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:5049";
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("שגיאת axios:", error.response ? error.response.data : error.message);
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/login.html";
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get('/items');
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    const newTask = { name, isComplete: false };
+    const result = await axios.post('/items', newTask);
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    const result = await axios.put(`/items/${id}`, { id, isComplete });
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    await axios.delete(`/items/${id}`);
+  }
+};
